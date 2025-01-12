71c984e0b529025d0f3603a0046bccc7f4cad155
Radi Halabi <radihalabi2010@hotmail.com>
2025-01-13 00:35:16 +0100

    new_Script
    Added Scrolling effect and buttons work now

---
+++ script.js
@@ -0,0 +1,137 @@
+$(document).ready(function() {
+  
+    var scrollLink = $('.scroll');
+    
+    // Smooth scrolling
+    scrollLink.click(function(e) {
+      e.preventDefault();
+      $('body,html').animate({
+        scrollTop: $(this.hash).offset().top
+      }, 1000 );
+    });
+    
+    // Active link switching
+    $(window).scroll(function() {
+      var scrollbarLocation = $(this).scrollTop();
+      
+      scrollLink.each(function() {
+        
+        var sectionOffset = $(this.hash).offset().top - 20;
+        
+        if ( sectionOffset <= scrollbarLocation ) {
+          $(this).parent().addClass('active');
+          $(this).parent().siblings().removeClass('active');
+        }
+      })
+      
+    })
+    
+  })
+  let hero = document.getElementById('hero-slides');
+let menu = document.getElementById('menu');
+let slides = document.getElementById('slides');
+let dribbble = document.getElementById('dribbble');
+let next = [ 'next', 'next-catch' ].map(n => document.getElementById(n));
+let prev = [ 'prev', 'prev-catch' ].map(n => document.getElementById(n));
+let slideChildren = slides.children;
+let slideCount = slides.children.length;
+let currentlyDemoing = false;
+let currentPage = 0;
+let slidesPerPage = () => window.innerWidth > 1700 ? 4 : window.innerWidth > 1200 ? 3 : 2;
+let maxPageCount = () => slideCount / slidesPerPage() - 1;
+
+function goToPage(pageNumber = 0) {
+	currentPage = Math.min(maxPageCount(), Math.max(0, pageNumber));
+	console.log(currentPage);
+	hero.style.setProperty('--page', currentPage);
+}
+
+function sleep(time) {
+	return new Promise(res => setTimeout(res, time));
+}
+
+function hoverSlide(index) {
+	index in slideChildren &&
+		slideChildren[index].classList.add('hover');
+}
+
+function unhoverSlide(index) {
+	index in slideChildren &&
+		slideChildren[index].classList.remove('hover');
+}
+
+async function demo() {
+	if(currentlyDemoing) {
+		return;
+	}
+	currentlyDemoing = true;
+	if(currentPage !== 0) {
+		goToPage(0);
+		await sleep(800);
+	}
+	let slides = slidesPerPage();
+	let pageSeq_ = { 2: [ 1, 2, 1 ], 3: [ 1, 2, 1 / 3 ], 4: [ 1, 1, 0 ] };
+	let pageSeq = pageSeq_[slides] || pageSeq_[4];
+	let slideSeq_ = { 2: [ 2, 4, 3 ], 3: [ 3, 6, 2 ], 4: [ 3, 6, 2 ] };
+	let slideSeq = slideSeq_[slides] || slideSeq_[2];
+	await sleep(300);
+	goToPage(pageSeq[0]);
+	await sleep(500);
+	hoverSlide(slideSeq[0]);
+	await sleep(1200);
+	goToPage(pageSeq[1]);
+	dribbble.classList.add('hover');
+	unhoverSlide(slideSeq[0]);
+	await sleep(500);
+	hoverSlide(slideSeq[1]);
+	await sleep(1200);
+	goToPage(pageSeq[2]);
+	unhoverSlide(slideSeq[1]);
+	await sleep(300);
+	hoverSlide(slideSeq[2]);
+	await sleep(1600);
+	goToPage(0);
+	unhoverSlide(slideSeq[2]);
+	dribbble.classList.remove('hover');
+	currentlyDemoing = false;
+}
+
+next.forEach(n => n.addEventListener('click', () => !currentlyDemoing && goToPage(currentPage + 1)));
+prev.forEach(n => n.addEventListener('click', () => !currentlyDemoing && goToPage(currentPage - 1)));
+menu.addEventListener('click', demo);
+
+sleep(100).then(demo);
+
+// window.addEventListener('resize', () => {
+	// console.log(document.body.style.getPropertyValue('--slide-per-page'));
+// });
+
+/* requestAnimationFrame */
+(function() {
+    $('.gallery-link').magnificPopup({
+      type: 'image',
+      closeOnContentClick: true,
+      closeBtnInside: false,
+      mainClass: 'mfp-with-zoom mfp-img-mobile',
+      image: {
+        verticalFit: true,
+        titleSrc: function(item) {
+          return item.el.find('figcaption').text() || item.el.attr('title');
+        }
+      },
+      zoom: {
+        enabled: true
+      },
+      // duration: 300
+      gallery: {
+        enabled: true,
+        navigateByImgClick: false,
+        tCounter: ''
+      },
+      disableOn: function() {
+        return $(window).width() > 640;
+      }
+    });
+  
+  }).call(this);
+  //slider bloggen 
